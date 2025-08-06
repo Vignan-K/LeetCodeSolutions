7a2efe72b3724f2f5705a0ac1d3a2dae533c7b59
Vignan017 <24211a05u0@bvrit.ac.in>
2025-08-06 06:17:26 +0000

    Sync LeetCode submission Runtime - 2238 ms (6.22%), Memory - 18.3 MB (100.00%)

---
+++ leetcode-problems/0001-two-sum/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int):
+        lis=[]
+        for i in range (0,len(nums)):
+            for j in range (i,len(nums)):
+                if i==j:
+                    continue
+                if nums[i]+nums[j]==target:
+                    lis.extend([i,j])
+                    break
+        return lis
+
+                    
+                
+                
+        
