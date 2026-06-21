9dc3119ecc8f8e05c92add417cf219c99cf154ef
Vignan017 <24211a05u0@bvrit.ac.in>
2026-06-21 12:53:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (64.45%)

---
+++ leetcode-problems/1013-fibonacci-number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a=0,b=1;
+        while (n>0){
+            int temp=a+b;
+            a=b;
+            b=temp;
+            n--;
+        }
+        return a;
+    }
+};
