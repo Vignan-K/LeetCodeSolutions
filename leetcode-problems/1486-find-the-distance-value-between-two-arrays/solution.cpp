91a419d925a302f354de5597451615d1907b9189
Vignan017 <24211a05u0@bvrit.ac.in>
2026-07-06 07:10:06 +0000

    Sync LeetCode submission Runtime - 14 ms (5.94%), Memory - 16.7 MB (51.65%)

---
+++ leetcode-problems/1486-find-the-distance-value-between-two-arrays/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int ans=0;
+        for (int i:arr1){
+            int count=0;
+            for (int j:arr2){
+                if (abs(i-j)<=d){
+                    count++;
+                }
+            }
+            if (count ==0) ans++;
+        }
+        return ans;
+    }
+};
