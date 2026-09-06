621f539b12dd5d4f97898acf933e5cc042551acc
Vignan017 <24211a05u0@bvrit.ac.in>
2026-09-06 09:52:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.7 MB (18.05%)

---
+++ leetcode-problems/2102-find-the-middle-index-in-array/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        vector<int> vtr(nums.size(),0);
+        int presum=0;
+        for (int i=0;i<nums.size();i++){
+            vtr[i]=presum;
+            presum+=nums[i];
+        }
+        int sufsum=0;
+        for (int i=nums.size()-1;i>=0;i--){
+            vtr[i]-=sufsum;
+            sufsum+=nums[i];
+        }
+        for (int i=0;i<nums.size();i++){
+            if (vtr[i]==0) return i;
+        }
+        return -1;
+    }
+};
